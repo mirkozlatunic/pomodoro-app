f2f563ba38afca87afebc3f2fa99828271356986
Mirko Zlatunic <m.zlatunic@gmail.com>
2024-09-06 21:53:01 -0700

    setup tailwind css and structure

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,25 @@
 import { createRoot } from 'react-dom/client';
+import Selection from './components/selection';
+import Timer from './components/timer';
+import Settings from './components/settings';
+
+import './index.css';
 
 const App = () => {
-  return <h1>Hello</h1>;
+  return (
+    <div>
+      <h1>pomodoro</h1>
+      <Selection />
+      <Timer />
+      <Settings />
+    </div>
+  );
 };
 
-const container = document.querySelector('#root');
-const root = createRoot(container!);
-
-root.render(<App />);
+const container = document.getElementById('root');
+if (container) {
+  const root = createRoot(container);
+  root.render(<App />);
+} else {
+  console.error('Root element not found');
+}
